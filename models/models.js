1c3d859e2da61173bf14cecfa7d6b2a19fc8ee40
nmarsden <nmarsden@gmail.com>
2012-05-18 23:19:16 +1000

    - /games/:id/goal/new  :  now adds goal to game - added underscore.js dependency for _.filter and _.find methods - added playerScore() and populateForWinner() methods to GameSchema

--- models/models.js
+++ models/models.js
@@ -1,4 +1,5 @@
-var mongoose = require('mongoose');
+var mongoose = require('mongoose'),
+    _ = require("../underscore-min");
 
 // Schemas
 var Schema = mongoose.Schema;
@@ -19,8 +20,26 @@ var GameSchema = new Schema({
     gameType: { type: String, enum: ["singles", "doubles"] },
     players: [{ type: ObjectId, ref: 'User' }],
     goals: [GoalSchema],
-    state: { type: String, enum: ["in-progress", "complete"] }
+    state: { type: String, enum: ["in-progress", "complete"] },
+    startDate: { type: Date, default: Date.now },
+    endDate: { type: Date },
+    winner: [{ type: ObjectId, ref: 'User' }],
+    loser: [{ type: ObjectId, ref: 'User' }]
 });
+
+GameSchema.methods.playerScore = function (playerId) {
+    return _.filter(this.goals, function(goal){
+        return goal.user.equals(playerId);
+    }).length;
+};
+
+GameSchema.methods.populateForWinner = function (playerId) {
+    this.state = "complete";
+    // TODO set endDate
+//game.endDate = Date.now;
+    this.winner = playerId;
+    this.loser = _.find(this.players, function(player){ return player._id != playerId; })._id;
+}
 
 // Models
 exports.User = mongoose.model('User', UserSchema);
--- routes/games.js
+++ routes/games.js
@@ -1,4 +1,5 @@
-var models = require('../models/models');
+var models = require('../models/models'),
+    _ = require("../underscore-min");
 
 module.exports = function(app){
     app.get('/games/new', function(req, res){
@@ -22,32 +23,104 @@ module.exports = function(app){
                 return console.log(err);
             }
         });
-        models.Game.findById(game._id).populate('players', ['name']).run(function (err, game) {
-            var playerName1 = game.players[0].name;
-            var playerName2 = game.players[1].name;
-            res.render('gameView', {
-                gameId: game._id,
-                playerName1: playerName1,
-                playerName2: playerName2,
-                playerScore1: req.param('playerScore1'),
-                playerScore2: req.param('playerScore2')
-            });
+        models.Game.findById(game._id)
+            .populate('players', ['name'])
+            .populate('goals', ['user'])
+            .run(function (err, game) {
+                var playerName1 = game.players[0].name;
+                var playerName2 = game.players[1].name;
+                var playerId1 = game.players[0]._id;
+                var playerId2 = game.players[1]._id;
+                res.render('gameView', {
+                    gameId: game._id,
+                    playerName1: playerName1,
+                    playerName2: playerName2,
+                    playerId1: playerId1,
+                    playerId2: playerId2,
+                    playerScore1: "0",
+                    playerScore2: "0"
+                });
         });
     });
 
     app.post('/games/:id/goal/new', function(req, res){
-        // TODO check if a player score has reached 10, and if so change game state to finished
-        models.Game.findById(req.params.id).populate('players', ['name']).run(function (err, game) {
-            var playerName1 = game.players[0].name;
-            var playerName2 = game.players[1].name;
-            res.render('gameView', {
-                gameId: game._id,
-                playerName1: playerName1,
-                playerName2: playerName2,
-                playerScore1: req.param('playerScore1'),
-                playerScore2: req.param('playerScore2')
+        // 1. find game
+        var gameId = req.params.id;
+        var playerId = req.param('playerId');
+        models.Game.findById(gameId)
+            .populate('players', ['name'])
+//            .populate('goals')
+            .run(function (playerId, err, game) {
+                if (err) {
+                    return console.log("ERROR RETRIEVING GAME", err);
+                }
+                if (game.state === "complete") {
+                    return console.log("cannot add goal! game already complete", err);
+                }
+                // 2. Determine if the player has just scored the winning goal
+                var playerGoals = game.playerScore(playerId) + 1;
+                console.log("playerGoals:", playerGoals);
+
+                if (playerGoals === 10) {
+                    console.log("GAME COMPLETE!");
+                    game.populateForWinner(playerId);
+                }
+
+                // 3. create goal
+                var goal = new models.Goal({
+                    user: playerId
+                });
+
+                // 4. save goal
+                goal.save(function (game, err, goal) {
+                    if (err) {
+                        return console.log("ERROR SAVING GOAL", err);
+                    }
+                    // 5. add new goal to game
+                    game.goals.push(goal);
+
+                    // 6. save game
+                    game.save(function (err, game) {
+                        if (err) {
+                            return console.log("ERROR SAVING GAME", err);
+                        }
+                        var playerName1 = game.players[0].name;
+                        var playerName2 = game.players[1].name;
+                        var playerId1 = game.players[0]._id;
+                        var playerId2 = game.players[1]._id;
+                        var playerScore1 = game.playerScore(playerId1);
+                        var playerScore2 = game.playerScore(playerId2);
+                        console.log("playerScore1:", playerScore1);
+                        console.log("playerScore2:", playerScore2);
+                        res.render('gameView', {
+                            gameId: game._id,
+                            playerName1: playerName1,
+                            playerName2: playerName2,
+                            playerId1: playerId1,
+                            playerId2: playerId2,
+                            playerScore1: playerScore1,
+                            playerScore2: playerScore2
+                        });
+                    });
+                }.bind(null, game));
+            }.bind(null, playerId));
+    });
+
+    app.get('/games/:id', function(req, res){
+        models.Game.findById(req.params.id)
+            .populate('players', ['name'])
+            .populate('goals')
+            .run(function (err, game) {
+                var playerName1 = game.players[0].name;
+                var playerName2 = game.players[1].name;
+                res.render('gameView', {
+                    gameId: game._id,
+                    playerName1: playerName1,
+                    playerName2: playerName2,
+                    playerScore1: "0",
+                    playerScore2: "0"
+                });
             });
-        });
     });
 
 //    app.get('/games', function(req, res){
