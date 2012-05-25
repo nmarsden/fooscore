f2247f6df59516dd15de6fd499bf0a3f3886de3c
nmarsden <nmarsden@gmail.com>
2012-05-25 21:58:34 +1000

    - ios5 bug: when using a select field, and other selects are on the same page, previous/next buttons are available to navigate between select fields.  The problem is that using the previous/next buttons does not fire change events.  This means that dynamically altering other selects can't be done on the change event. - to work around this, other selects are disabled on focus to prevent the use of previous/next and re-enabled on blur

--- public/javascripts/gameNew.js
+++ public/javascripts/gameNew.js
@@ -3,13 +3,33 @@ pageScript(function($context){
 
         var userSelectedDatas = _.map($context.find('select'), function(userSelect) {
             var $userSelect = $(userSelect);
-            return { $userSelect: $userSelect, id: $userSelect.get()[0].id, selectedValue: $userSelect.find("option:selected").val() }
+            return {
+                $userSelect: $userSelect,
+                id: $userSelect.get()[0].id,
+                selectedValue: $userSelect.find("option:selected").val()
+            }
         });
+        var ios5 = navigator.userAgent.match(/OS 5_[0-9_]+ like Mac OS X/i) != null;
+
         var userDatas = $.data($('#newGameContent')[0], 'users');
 
+        var disableOtherUserSelects = function(event, ui) {
+            _.each(userSelectedDatas, function(userSelectedData) {
+                if (event.target.id !== userSelectedData.id) {
+                    userSelectedData.$userSelect.prop("disabled", true);
+                }
+            });
+        };
+
+        var enableOtherUserSelects = function(event, ui) {
+            _.each(userSelectedDatas, function(userSelectedData) {
+                if (event.target.id !== userSelectedData.id) {
+                    userSelectedData.$userSelect.prop("disabled", false);
+                }
+            });
+        };
 
         var userChangeEventHandler = function(event, ui) {
-
             // Update userSelectedDatas
             var targetSelectedValue = $(event.target).find("option:selected").val();
             _.each(userSelectedDatas, function(userSelectedData) {
@@ -49,10 +69,17 @@ pageScript(function($context){
 
             // TODO disable Start button until all users are selected
 
-        }
+        };
 
         _.each(userSelectedDatas, function(userSelectedData) {
             userSelectedData.$userSelect.bind("change", userChangeEventHandler);
+            if (ios5) {
+                // ios5 bug fix: clicking previous/next buttons does not fire change events
+                // so the following disables other selects to prevent the use of previous/next
+                userSelectedData.$userSelect.bind("focus", disableOtherUserSelects);
+                userSelectedData.$userSelect.bind("blur", enableOtherUserSelects);
+            }
         });
+
     });
 });
